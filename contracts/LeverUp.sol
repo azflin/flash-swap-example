25d0a16564c7b5578fd01812e87ed6c4412925ce
AzFlin <azflin@users.noreply.github.com>
2022-07-18 16:06:51 -0400

    continue

--- contracts/LeverUp.sol
+++ contracts/LeverUp.sol
@@ -11,11 +11,14 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 address constant EULER_MARKETS = 0x3520d5a913427E6F0D6A83E07ccD4A4da316e4d3;
 address constant WETH_ETOKEN = 0x1b808F49ADD4b8C6b5117d9681cF7312Fcf0dC1D;
+address constant USDC_DTOKEN = 0x84721A3dB22EB852233AEAE74f9bC8477F8bcc42;
 address constant WETH = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
+address constant USDC = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
 address constant EULER_MAINNET = 0x27182842E098f60e3D576794A5bFFb0777E025d3;
 
 interface IEulerMarkets {
     function underlyingToEToken(address underlying) external view returns (address);
+    function enterMarket(uint subAccountId, address newMarket) external;
 }
 
 interface IEulerEToken {
@@ -23,6 +26,11 @@ interface IEulerEToken {
     function balanceOf(address account) external view returns (uint);
     function balanceOfUnderlying(address account) external view returns (uint);
     function withdraw(uint subAccountId, uint amount) external;
+}
+
+interface IEulerDToken {
+    function borrow(uint subAccountId, uint amount) external;
+    function balanceOf(address account) external view returns (uint);
 }
 
 contract LeverUp is IUniswapV3SwapCallback {
@@ -33,11 +41,24 @@ contract LeverUp is IUniswapV3SwapCallback {
         int256 amount1Delta,
         bytes calldata data
     ) external override {
+        // Approve Euler to withdraw WETH
         IERC20(WETH).approve(EULER_MAINNET, type(uint).max);
-        uint wethToDeposit = abi.decode(data, (uint));
-        console.log("wethToDeposit");
-        console.logUint(wethToDeposit);
-//        IEulerEToken(WETH_ETOKEN).deposit(0, )
+
+        // Deposit WETH and get back eTokens
+        (uint wethToDeposit, address pool)  = abi.decode(data, (uint, address));
+        IEulerEToken wethEToken = IEulerEToken(WETH_ETOKEN);
+        wethEToken.deposit(0, wethToDeposit);
+
+        // Enter WETH market
+        IEulerMarkets markets = IEulerMarkets(EULER_MARKETS);
+        markets.enterMarket(0, WETH);
+
+        // Borrow 100 USDC
+        IEulerDToken usdcDToken = IEulerDToken(USDC_DTOKEN);
+        usdcDToken.borrow(0, uint256(amount0Delta));
+
+        // Send 100 USDC back to v3 pool
+        IERC20(USDC).transfer(pool, uint256(amount0Delta));
     }
 
     function leverUp(IUniswapV3Pool pool, uint256 amountWeth, uint256 amountUsdcSpecified) external {
@@ -49,7 +70,7 @@ contract LeverUp is IUniswapV3SwapCallback {
             true,
             amountUsdcSpecified.toInt256(),
             TickMath.MIN_SQRT_RATIO + 1,
-            abi.encode(amountWeth)
+            abi.encode(amountWeth, pool)
         );
     }
 }
--- test/LeverUp-test.js
+++ test/LeverUp-test.js
@@ -2,6 +2,52 @@ const { expect } = require("chai");
 const { ethers } = require("hardhat");
 const hre = require("hardhat");
 
+const ERC20_ABI = [
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "spender",
+        "type": "address"
+      },
+      {
+        "internalType": "uint256",
+        "name": "amount",
+        "type": "uint256"
+      }
+    ],
+    "name": "approve",
+    "outputs": [
+      {
+        "internalType": "bool",
+        "name": "",
+        "type": "bool"
+      }
+    ],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "account",
+        "type": "address"
+      }
+    ],
+    "name": "balanceOf",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+];
+
 describe("LeverUp", function () {
   it("base case", async function () {
     await hre.network.provider.request({
@@ -28,329 +74,32 @@ describe("LeverUp", function () {
       "0xEF330d6F0B4375c39D8eD3d0D690a5B69e9EcD0c"
     );
     leverUp = leverUp.connect(signer);
+    console.log("leverUp.address", leverUp.address);
 
     let weth = new ethers.Contract(
       "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2",
-      [
-        {
-          "inputs": [
-            {
-              "internalType": "string",
-              "name": "name_",
-              "type": "string"
-            },
-            {
-              "internalType": "string",
-              "name": "symbol_",
-              "type": "string"
-            },
-            {
-              "internalType": "uint256",
-              "name": "amountToMint",
-              "type": "uint256"
-            }
-          ],
-          "stateMutability": "nonpayable",
-          "type": "constructor"
-        },
-        {
-          "anonymous": false,
-          "inputs": [
-            {
-              "indexed": true,
-              "internalType": "address",
-              "name": "owner",
-              "type": "address"
-            },
-            {
-              "indexed": true,
-              "internalType": "address",
-              "name": "spender",
-              "type": "address"
-            },
-            {
-              "indexed": false,
-              "internalType": "uint256",
-              "name": "value",
-              "type": "uint256"
-            }
-          ],
-          "name": "Approval",
-          "type": "event"
-        },
-        {
-          "anonymous": false,
-          "inputs": [
-            {
-              "indexed": true,
-              "internalType": "address",
-              "name": "from",
-              "type": "address"
-            },
-            {
-              "indexed": true,
-              "internalType": "address",
-              "name": "to",
-              "type": "address"
-            },
-            {
-              "indexed": false,
-              "internalType": "uint256",
-              "name": "value",
-              "type": "uint256"
-            }
-          ],
-          "name": "Transfer",
-          "type": "event"
-        },
-        {
-          "inputs": [
-            {
-              "internalType": "address",
-              "name": "owner",
-              "type": "address"
-            },
-            {
-              "internalType": "address",
-              "name": "spender",
-              "type": "address"
-            }
-          ],
-          "name": "allowance",
-          "outputs": [
-            {
-              "internalType": "uint256",
-              "name": "",
-              "type": "uint256"
-            }
-          ],
-          "stateMutability": "view",
-          "type": "function"
-        },
-        {
-          "inputs": [
-            {
-              "internalType": "address",
-              "name": "spender",
-              "type": "address"
-            },
-            {
-              "internalType": "uint256",
-              "name": "amount",
-              "type": "uint256"
-            }
-          ],
-          "name": "approve",
-          "outputs": [
-            {
-              "internalType": "bool",
-              "name": "",
-              "type": "bool"
-            }
-          ],
-          "stateMutability": "nonpayable",
-          "type": "function"
-        },
-        {
-          "inputs": [
-            {
-              "internalType": "address",
-              "name": "account",
-              "type": "address"
-            }
-          ],
-          "name": "balanceOf",
-          "outputs": [
-            {
-              "internalType": "uint256",
-              "name": "",
-              "type": "uint256"
-            }
-          ],
-          "stateMutability": "view",
-          "type": "function"
-        },
-        {
-          "inputs": [],
-          "name": "decimals",
-          "outputs": [
-            {
-              "internalType": "uint8",
-              "name": "",
-              "type": "uint8"
-            }
-          ],
-          "stateMutability": "view",
-          "type": "function"
-        },
-        {
-          "inputs": [
-            {
-              "internalType": "address",
-              "name": "spender",
-              "type": "address"
-            },
-            {
-              "internalType": "uint256",
-              "name": "subtractedValue",
-              "type": "uint256"
-            }
-          ],
-          "name": "decreaseAllowance",
-          "outputs": [
-            {
-              "internalType": "bool",
-              "name": "",
-              "type": "bool"
-            }
-          ],
-          "stateMutability": "nonpayable",
-          "type": "function"
-        },
-        {
-          "inputs": [
-            {
-              "internalType": "address",
-              "name": "spender",
-              "type": "address"
-            },
-            {
-              "internalType": "uint256",
-              "name": "addedValue",
-              "type": "uint256"
-            }
-          ],
-          "name": "increaseAllowance",
-          "outputs": [
-            {
-              "internalType": "bool",
-              "name": "",
-              "type": "bool"
-            }
-          ],
-          "stateMutability": "nonpayable",
-          "type": "function"
-        },
-        {
-          "inputs": [],
-          "name": "name",
-          "outputs": [
-            {
-              "internalType": "string",
-              "name": "",
-              "type": "string"
-            }
-          ],
-          "stateMutability": "view",
-          "type": "function"
-        },
-        {
-          "inputs": [
-            {
-              "internalType": "address",
-              "name": "to",
-              "type": "address"
-            },
-            {
-              "internalType": "uint256",
-              "name": "amount",
-              "type": "uint256"
-            }
-          ],
-          "name": "setBalance",
-          "outputs": [],
-          "stateMutability": "nonpayable",
-          "type": "function"
-        },
-        {
-          "inputs": [],
-          "name": "symbol",
-          "outputs": [
-            {
-              "internalType": "string",
-              "name": "",
-              "type": "string"
-            }
-          ],
-          "stateMutability": "view",
-          "type": "function"
-        },
-        {
-          "inputs": [],
-          "name": "totalSupply",
-          "outputs": [
-            {
-              "internalType": "uint256",
-              "name": "",
-              "type": "uint256"
-            }
-          ],
-          "stateMutability": "view",
-          "type": "function"
-        },
-        {
-          "inputs": [
-            {
-              "internalType": "address",
-              "name": "to",
-              "type": "address"
-            },
-            {
-              "internalType": "uint256",
-              "name": "amount",
-              "type": "uint256"
-            }
-          ],
-          "name": "transfer",
-          "outputs": [
-            {
-              "internalType": "bool",
-              "name": "",
-              "type": "bool"
-            }
-          ],
-          "stateMutability": "nonpayable",
-          "type": "function"
-        },
-        {
-          "inputs": [
-            {
-              "internalType": "address",
-              "name": "from",
-              "type": "address"
-            },
-            {
-              "internalType": "address",
-              "name": "to",
-              "type": "address"
-            },
-            {
-              "internalType": "uint256",
-              "name": "amount",
-              "type": "uint256"
-            }
-          ],
-          "name": "transferFrom",
-          "outputs": [
-            {
-              "internalType": "bool",
-              "name": "",
-              "type": "bool"
-            }
-          ],
-          "stateMutability": "nonpayable",
-          "type": "function"
-        }
-      ],
+      ERC20_ABI,
       ethers.provider
     );
     weth = weth.connect(signer);
+
+    const weth_etoken = new ethers.Contract(
+      "0x1b808F49ADD4b8C6b5117d9681cF7312Fcf0dC1D",
+      ERC20_ABI,
+      ethers.provider
+    );
+
     await weth.approve(leverUp.address, "115792089237316195423570985008687907853269984665640564039457584007913129639935");
 
     await leverUp.leverUp(
       "0x8ad599c3a0ff1de082011efddc58f1908eb6e6d8",
+      // Deposit 1 WETH
       "1000000000000000000",
+      // 100 USDC to borrow
       "100000000"
     );
+
+    console.log("WETH EToken Balance: ", await weth_etoken.balanceOf("0xAe120F0df055428E45b264E7794A18c54a2a3fAF"));
+    console.log("WETH Balance: ", await weth.balanceOf("0xAe120F0df055428E45b264E7794A18c54a2a3fAF"));
   });
 });
